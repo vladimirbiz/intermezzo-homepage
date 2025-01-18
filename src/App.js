f3b87bf169c4bf0470185a2af70478e452c87f64
Vladimir Tasevski Macbook Air <vladimir@tasevskimarketingagency.com>
2025-01-18 15:17:36 +0100

    +

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <a href='https://www.facebook.com'>
         <div className='div-h3'>
         <h3 className='h1-day'>INTERMEZZO BY DAY</h3>
-        <h3 className='h1-day-mob'>BY DAY</h3>
+        <h4 className='h1-day-mob'>BY DAY</h4>
         </div>
         </a>
       </div>
@@ -16,7 +16,7 @@ function App() {
         <a href='https://www.instagram.com'>
         <div className='div-h3'>
         <h3 className='h1-night'>INTERMEZZO BY NIGHT</h3>
-        <h3 className='h1-night-mob'>BY NIGHT</h3>
+        <h4 className='h1-night-mob'>BY NIGHT</h4>
         </div>
         </a>
       </div>
