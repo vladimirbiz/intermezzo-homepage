7d25b86b576a6807664b2fc892357a947c0734fa
Vladimir Tasevski Macbook Air <vladimir@tasevskimarketingagency.com>
2025-01-18 14:01:28 +0100

    +

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="split-screen">
+      <div className="left-side">
+        <a href='https://www.facebook.com'>
+        <div className='div-h3'>
+        <h3 className='h1-day'>INTERMEZZO BY DAY</h3>
+        </div>
+        </a>
+      </div>
+      <div className="right-side">
+        <a href='https://www.instagram.com'>
+        <div className='div-h3'>
+        <h3 className='h1-night'>INTERMEZZO BY NIGHT</h3>
+        </div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
