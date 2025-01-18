b6ff60aa6c833f3d1e2c97952148043a44da7040
Vladimir Tasevski Macbook Air <vladimir@tasevskimarketingagency.com>
2025-01-18 15:42:03 +0100

    +added website urls

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="split-screen">
       <div className="left-side">
-        <a href='https://www.facebook.com'>
+        <a href='https://mediumseagreen-chinchilla-m6ljjnwqxjtoogob.builder-preview.com/'>
         <div className='div-h3'>
         <h3 className='h1-day'>INTERMEZZO BY DAY</h3>
         <h4 className='h1-day-mob'>DAY</h4>
@@ -13,7 +13,7 @@ function App() {
         </a>
       </div>
       <div className="right-side">
-        <a href='https://www.instagram.com'>
+        <a href='https://intermezzo-music-rituals-amqb1b6rvqcwepbk.builder-preview.com/'>
         <div className='div-h3'>
         <h3 className='h1-night'>INTERMEZZO BY NIGHT</h3>
         <h4 className='h1-night-mob'>NIGHT</h4>
